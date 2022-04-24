5f5c7415f930872eb4b6adce5cf9108576105a1e
Victor <thevictorxx@gmail.com>
2022-04-24 20:28:17 -0300

    ADD: Nueva forma de descarga y separacion de videos

--- src/routes/main.routes.js
+++ src/routes/main.routes.js
@@ -10,20 +10,32 @@ router.get('/', (req, res) => {
 
 router.get('/url/:url', async (req, res) => {
   const { formats, videoDetails } = await getInfoVideo(req.query.url)
+  const filteredFormarts = formats.filter((format) => {
+    return format.audioQuality !== undefined
+  })
+  const mutedFormats = formats.filter((format) => {
+    return format.audioQuality === undefined
+  })
   const videoData = {
     title: videoDetails.title,
     urlImg: videoDetails.thumbnails[videoDetails.thumbnails.length - 1].url,
     thumbnail: videoDetails.thumbnail,
-    formats
+    filteredFormarts,
+    mutedFormats,
+    url: req.query.url
   }
   res.render('videoInfo', { videoData })
 })
 
 router.get('/download', async (req, res) => {
   const URL = req.query.URL
-  res.header('Content-Disposition', 'attachment; filename="video.mp4"')
+  const ITAG = req.query.ITAG
+  const EXT = req.query.EXT
+  const { videoDetails } = await getInfoVideo(URL)
+  const nombreArchivo = videoDetails.title + '.' + EXT.split(';')[0].split('/')[1]
+  res.header('Content-Disposition', `attachment; filename="${nombreArchivo}"`)
   ytdl(URL, {
-    format: 'mp4'
+    quality: ITAG
   }).pipe(res)
 })
 
