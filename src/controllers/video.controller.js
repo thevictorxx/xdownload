ceaa08611b6e74559de9c921255068aa23e9140a
Victor <thevictorxx@gmail.com>
2022-05-01 15:18:04 -0300

    FIX: No colapsa con urls invalidas

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -2,22 +2,28 @@ const ytdl = require('ytdl-core')
 const getInfoVideo = require('../utils/getVideoInfo')
 
 const getInfo = async (req, res) => {
-  const { formats, videoDetails } = await getInfoVideo(req.query.url)
-  const filteredFormarts = formats.filter((format) => {
-    return format.audioQuality !== undefined
-  })
-  const mutedFormats = formats.filter((format) => {
-    return format.audioQuality === undefined
-  })
-  const videoData = {
-    title: videoDetails.title,
-    urlImg: videoDetails.thumbnails[videoDetails.thumbnails.length - 1].url,
-    thumbnail: videoDetails.thumbnail,
-    filteredFormarts,
-    mutedFormats,
-    url: req.query.url
+  try {
+    const { formats, videoDetails } = await getInfoVideo(req.query.url)
+    const filteredFormarts = formats.filter((format) => {
+      return format.audioQuality !== undefined
+    })
+    const mutedFormats = formats.filter((format) => {
+      return format.audioQuality === undefined
+    })
+    const videoData = {
+      title: videoDetails.title,
+      urlImg: videoDetails.thumbnails[videoDetails.thumbnails.length - 1].url,
+      thumbnail: videoDetails.thumbnail,
+      filteredFormarts,
+      mutedFormats,
+      url: req.query.url
+    }
+    res.render('videoInfo', { videoData })
+  } catch (error) {
+    if (error) {
+      res.redirect('/?format')
+    }
   }
-  res.render('videoInfo', { videoData })
 }
 
 const getDownload = async (req, res) => {
